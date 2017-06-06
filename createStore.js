d3a0360471f545d9959c88927946c13472b43d81
Topppy <taoyiming2010@126.com>
2017-06-06 15:01:51 +0800

    Create createStore.js

---
+++ createStore.js
@@ -0,0 +1,5 @@
+```
+import isPlainObject from 'lodash/isPlainObject'
+```
+
+> Import a function to checks if `argument` is a plain object, that is, an object created by the * `Object` constructor or one with a `[[Prototype]]` of `null`.
